3a8655a7299f0b9e5a16cabff120c1ae577767b7
kaylambaker <kaylambaker456@gmail.com>
2022-09-15 13:13:12 -0500

    grade parser with changes made according to Ahad's reivew

--- parser/parser.go
+++ parser/parser.go
@@ -6,8 +6,10 @@ package main
 import (
 	"context"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"parser/configs"
+	"path/filepath"
 	"reflect"
 
 	"os"
@@ -27,41 +29,30 @@ type Class struct {
 	gradeDistribution bson.A
 }
 
-func main() {
-	URI := configs.EnvMongoURI()
-	if len(os.Args) != 3 {
-		fmt.Println("usage: \ngo build parser.go\n./parser.exe [file_path] [academic_session]\nexample: \ngo build parser.go\n./parser.exe \"Fall 2019.csv\" 19F")
-	}
-	filePath := os.Args[1]
-	academicSession := os.Args[2]
-	var classes []Class
-	file, err := os.Open(filePath)
+func getCollection(client *mongo.Client, database string, collection string) (returnCollection *mongo.Collection) {
+	return (client.Database(database).Collection(collection))
+}
+
+func DBConnect(URI string) (client *mongo.Client) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(URI))
 	if err != nil {
-		fmt.Println("could not open file " + filePath)
 		fmt.Println(err)
-		return
+		panic(err)
 	}
-	reader := csv.NewReader(file)
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	return client
+}
+
+func csvToClassesSlice(csvFile *os.File, logFile *os.File) (classes []Class) {
+	reader := csv.NewReader(csvFile)
 	records, err := reader.ReadAll() // records is [][]strings
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
-	// create logs directory
-	if _, err := os.Stat("logs"); err != nil {
-		os.Mkdir("logs", os.ModePerm)
-	}
-	str := strings.Split(filePath, "/")
-	if len(str) == 1 {
-		str = strings.Split(filePath, "\\")
-	}
-	logFile, err := os.Create("logs/" + strings.Split(str[len(str)-1], ".")[0] + ".log")
-	if err != nil {
-		fmt.Println("could not create log file")
-		fmt.Println(err)
-		return
-	}
-
 	// look for the subject column and w column
 	subjectCol := -1
 	wCol := -1
@@ -107,27 +98,21 @@ func main() {
 				gradeDistribution: tempSlice,
 			})
 	}
-	// connect to monodb //
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(URI))
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-	coursesCollection := client.Database("combinedDB").Collection("courses")
-	sectionsCollection := client.Database("combinedDB").Collection("sections")
-	// -- //
+	return classes
+}
+
+// inserts grades into mongodb
+func insertGrades(sectionsCollection *mongo.Collection, coursesCollection *mongo.Collection, classes []Class, academicSession string, logFile *os.File) {
 	for i := 0; i < len(classes); i++ {
 		var result bson.D
+		var err error
 		err = coursesCollection.FindOne(context.TODO(), bson.D{{"course_number", classes[i].catalogNumber}, {"subject_prefix", classes[i].subject}}).Decode(&result)
 		// if class is not in courses section
 		if err != nil {
 			// log that class could not be found
 			logFile.WriteString("could not find course " + classes[i].subject + " " + classes[i].catalogNumber + "\n")
 			fmt.Println("could not find course " + classes[i].subject + " " + classes[i].catalogNumber)
+			fmt.Println(err)
 		} else {
 			mymap := result.Map()
 			section_ids := reflect.ValueOf(mymap["sections"])
@@ -135,7 +120,7 @@ func main() {
 				result = nil
 				idStr := fmt.Sprint(section_ids.Index(j).Elem())
 				idStr = strings.Split(idStr, "\"")[1]
-				idObj, _ := primitive.ObjectIDFromHex(idStr)
+				idObj, _ := primitive.ObjectIDFromHex(idStr) // can't use id string, ahs to be object
 				err := sectionsCollection.FindOne(context.TODO(), bson.D{{"_id", idObj}}).Decode(&result)
 				if err != nil {
 					fmt.Println(err)
@@ -158,4 +143,45 @@ func main() {
 			}
 		}
 	}
+}
+
+func main() {
+	URI := configs.EnvMongoURI()
+	helpMsg := "usage: \ngo run parser.go -file [file_path] -semester [academic_session]\nexamples: \ngo run parser.go -file \"Fall 2019.csv\" -semester 19F\ngo run parser.go -file \"Summer 2019.csv\" -semester 19U"
+	fileFlag := flag.String("file", "", "csv grade file to be parsed")
+	semesterFlag := flag.String("semester", "", "semester of the grades")
+	helpFlag := flag.Bool("help", false, "if user needs help")
+	flag.Parse()
+	if *helpFlag == true || *fileFlag == "" || *semesterFlag == "" {
+		fmt.Println(helpMsg)
+		os.Exit(1)
+	}
+	csvPath := *fileFlag
+	academicSession := *semesterFlag
+	csvFile, err := os.Open(csvPath)
+	if err != nil {
+		fmt.Println("could not open file " + csvPath)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// create logs directory
+	if _, err := os.Stat("logs"); err != nil {
+		os.Mkdir("logs", os.ModePerm)
+	}
+	// create log file [name of csv].log in logs directory
+	logFileName := filepath.Base(csvPath)
+	logFile, err := os.Create("logs/" + logFileName + ".log")
+	if err != nil {
+		fmt.Println("could not create log file")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// put class data from csv into classes slice
+	classes := csvToClassesSlice(csvFile, logFile)
+	client := DBConnect(URI)
+
+	// insert grades into mongodb
+	insertGrades(getCollection(client, "combinedDB", "sections"), getCollection(client, "combinedDB", "courses"), classes, academicSession, logFile)
 }
